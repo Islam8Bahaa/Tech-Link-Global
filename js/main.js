c46e3c142cfc5a95b90ce7ce5fc89aa77e4657a5
Islam8Bahaa <81769785+Islam8Bahaa@users.noreply.github.com>
2023-12-13 15:44:03 +0200

    New

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.main-menu').slicknav({
+        prependTo:".mobile-nav",
+        label: '',
+        duration: 500,
+        easingOpen: "easeOutBounce",
+    });
+});
