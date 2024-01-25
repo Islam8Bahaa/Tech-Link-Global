62a5e463d73f2b7b23e57d58bbbdba8449eb6302
Islam8Bahaa <81769785+Islam8Bahaa@users.noreply.github.com>
2024-01-25 15:02:49 +0200

    Done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-    $('#header').load('header.html');
-    $('#footer').load('footer.html');
-    
-    $('.main-menu').slicknav({
-        prependTo:".mobile-nav",
-        label: '',
-        duration: 500,
-        easingOpen: "easeOutBounce",
+    $('#header').load('header.html', function() {
+        // Code to execute after the header is loaded
+        $('.main-menu').slicknav({
+            prependTo: ".mobile-nav",
+            label: '',
+            duration: 500,
+            easingOpen: "easeOutBounce",
+        });
     });
+    $('#footer').load('footer.html');
     
 
 });
