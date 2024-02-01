1a878f8309aac42139cd326f554c42fbb9d83367
Islam8Bahaa <81769785+Islam8Bahaa@users.noreply.github.com>
2024-02-02 01:44:08 +0200

    Done

--- js/main.js
+++ js/main.js
@@ -10,5 +10,34 @@ $(document).ready(function() {
     });
     $('#footer').load('footer.html');
 
-
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 0,
+          },
+          "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 0,
+          },
+          "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 0,
+          },
+          "@1.50": {
+            slidesPerView: 4,
+            spaceBetween: 0,
+          },
+        },
+      });
 });
